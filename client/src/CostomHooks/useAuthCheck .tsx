1af835faca0a1b2d64f490ce0a3dbc3846d3bf52
SiyadMuhsin <siyadsm4065@gmail.com>
2025-04-27 08:31:15 +0530

    avoiding any usage

--- client/src/CostomHooks/useAuthCheck .tsx
+++ client/src/CostomHooks/useAuthCheck .tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import Cookie from "js-cookie";
-
 import { useDispatch } from "react-redux";
 import { changeRole, loginSuccess, logout } from "../Slice/authSlice";
 import { userAPI, adminAPI } from "../../config/axiosConfig";
@@ -10,18 +8,16 @@ import { toast } from "react-toastify";
 
 const useAuthCheck = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchCurrentUser = async () => {
       try {
-        // ✅ Fetch user info
         const response = await userAPI.get("/auth/me", {
           withCredentials: true,
         });
         if (response.data.success) {
           let role = getRoleFromToken();
-          console.log(role)
+
           if (role === "expert") {
             dispatch(changeRole("expert"));
           }
