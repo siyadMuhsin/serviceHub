dfb76989e8b14d5c910198143b834a5317051a2a
SiyadMuhsin <siyadsm4065@gmail.com>
2025-04-30 22:12:50 +0530

    cookies configuration

--- server/src/utils/setAuthCookies .ts
+++ server/src/utils/setAuthCookies .ts
@@ -4,14 +4,14 @@ export const setAuthCookies = (res: Response, accessToken: string, refreshToken:
     res.cookie("accessToken", accessToken, {
       httpOnly: false, 
       secure: process.env.NODE_ENV === 'production', 
-      sameSite: 'strict',
+      sameSite: 'lax',
       maxAge: 15 * 60 * 1000, // 15 minutes
     });
   
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
   };
